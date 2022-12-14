f07bb96d21608678e52d88489e53ee55f73a1dfe
حسين مصطفي محمد صلاح الدين <14712020101885@stud.cu.edu.eg>
2022-12-14 23:16:45 +0200

    pawn choices semi working

--- DrawB.asm
+++ DrawB.asm
@@ -241,8 +241,9 @@ MAIN PROC FAR
 
 ;calling function to choose a piece
     mov success,0 
-   choosepiece PrimaryC,SecondaryC,chezzP,chezzT,chezzC,playertpye,moveavailc,takeavailc,prevR,prevC,success,begr,begc,endr,endc,res
-
+    
+    choosepiece PrimaryC,SecondaryC,chezzP,chezzT,chezzC,playertpye,moveavailc,takeavailc,prevR,prevC,success,begr,begc,endr,endc,res
+   
     cmp success,0
     je suc
     mov selected,1
